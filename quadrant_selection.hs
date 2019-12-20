e0cae7dd2086deb5f0a340582044f6df3a65b653
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-12-20 01:32:32 -0700

    quadrant selection

---
+++ quadrant_selection.hs
@@ -0,0 +1,9 @@
+solve :: [Int] -> String
+solve [x, y]
+  | x > 0 && y > 0 = "1"
+  | x < 0 && y > 0 = "2"
+  | x < 0 && y < 0 = "3"
+  | x > 0 && y < 0 = "4"
+
+main :: IO()
+main = interact $ solve . map read . words
